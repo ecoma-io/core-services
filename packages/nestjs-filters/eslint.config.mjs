27544e49335c798f473f7633ccd2667c7d70edd5
John Martin <github-actions[bot]@users.noreply.github.com>
2025-11-13 11:53:25 +0000

    feat(nestjs-filters): add global exception filter for standardize output

---
+++ packages/nestjs-filters/eslint.config.mjs
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import baseConfig from '../../eslint.config.mjs';
+export default [
+  ...baseConfig,
+  {
+    files: ['**/*.json'],
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        {
+          ignoredFiles: [
+            '{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}',
+            '{projectRoot}/esbuild.config.{js,ts,mjs,mts}',
+          ],
+        },
+      ],
+    },
+  },
+];
---
+++ packages/nestjs-filters/jest.config.mjs
@@ -0,0 +1,4 @@
+export default {
+  displayName: 'nestjs-filters',
+  preset: '../../jest.preset.js',
+};
